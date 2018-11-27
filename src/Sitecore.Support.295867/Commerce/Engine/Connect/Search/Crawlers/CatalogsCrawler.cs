3bc304897f1b24a3dbb9ff443df2dfb71646e79a
DK\AMCH <amch@sitecore.net>
2018-11-27 14:52:33 +0200

    commit initial code

---
+++ src/Sitecore.Support.295867/Commerce/Engine/Connect/Search/Crawlers/CatalogsCrawler.cs
@@ -0,0 +1,53 @@
+﻿using Sitecore.Diagnostics;
+using System.Collections.Generic;
+using Sitecore.Commerce.Plugin.ManagedLists;
+using Sitecore.Commerce.Plugin.Catalog;
+using Sitecore.Commerce.Engine.Connect;
+using Sitecore.Commerce.Engine.Connect.Search;
+using Sitecore.Commerce.Engine.Connect.Search.Models;
+using Sitecore.Data;
+using Sitecore.Commerce.Engine.Connect.Search.Crawlers;
+
+namespace Sitecore.Support.Commerce.Engine.Connect.Search.Crawlers
+{
+    public class CatalogsCrawler : CatalogCrawlerBase<Catalog>
+    {
+        private static readonly List<ID> InternalTemplateIds = new List<ID>
+        {
+            CommerceConstants.KnownTemplateIds.CommerceCatalogTemplate,
+        };
+
+        protected override IEnumerable<ID> IndexableTemplateIds => InternalTemplateIds;
+
+        protected override ManagedList GetCatalogEntitiesToIndex(string environment, string listName, int itemsToSkip, int itemsToTake)
+        {
+            var itemsList = IndexUtility.GetCatalogsToIndex(environment, listName, itemsToSkip, itemsToTake);
+            return itemsList;
+        }
+
+        protected override List<CommerceCatalogIndexableItem> GetItemsToIndex(ManagedList searchResults, Dictionary<string, bool> mappedCatalogsCache)
+        {
+            Assert.ArgumentNotNull(searchResults, nameof(searchResults));
+            Assert.ArgumentNotNull(mappedCatalogsCache, nameof(mappedCatalogsCache));
+
+            var indexableList = new List<CommerceCatalogIndexableItem>();
+            foreach (Catalog catalog in searchResults.Items)
+            {
+                var isMapped = false;
+                if (!mappedCatalogsCache.TryGetValue(catalog.SitecoreId, out isMapped))
+                {
+                    var catalogPathIdList = this.Repository.GetPathIdsForSitecoreId(catalog.SitecoreId);
+                    isMapped = (catalogPathIdList != null && catalogPathIdList.Count > 0);
+                    mappedCatalogsCache[catalog.SitecoreId] = isMapped;
+                }
+
+                if (isMapped)
+                {
+                    indexableList.Add(this.TryGetItem(catalog, catalog.SitecoreId));
+                }
+            }
+
+            return indexableList;
+        }
+    }
+}
---
+++ src/Sitecore.Support.295867/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.295867")]
+[assembly: AssemblyProduct("Sitecore.Support.295867")]
+[assembly: ComVisible(false)]
